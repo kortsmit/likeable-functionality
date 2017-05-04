256e146705ca310f88f7c839c30ca7f2d32330cb
Tim Kortsmit <tim@kortsmit.com>
2017-05-03 21:09:42 -0400

    Post display with footer functions

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,19 +4,26 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Posts</div>
-
-                    <div class="panel-body">
-                        @foreach($posts as $post)
-                            <article>
-                                <h4>{{ $post->title }}</h4>
-                                <div class="body">{{ $post->body }}</div>
-                                <hr>
-                            </article>
-                        @endforeach
-                    </div>
-                </div>
+                @foreach($posts as $post)
+                    <article>
+                        <div class="panel panel-default">
+                            <div class="panel-heading">{{ $post->title }}</div>
+                            <div class="panel-body">
+                                {{ $post->body }}
+                            </div>
+                            <div class="panel-footer">
+                                <i class="fa fa-clock-o"></i>
+                                <small>{{ $post->created_at->diffForHumans() }}</small>
+                                <span class="pull-right">
+                                    <a href="">
+                                        <i class="fa fa-heart"></i>
+                                        <small>123</small>
+                                    </a>
+                                </span>
+                            </div>
+                        </div>
+                    </article>
+                @endforeach
             </div>
         </div>
     </div>
